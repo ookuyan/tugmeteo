09a765dfe5cf191de6c2b08dbf611d503d5ed015
ookuyan <ookuyan@gmail.com>
2019-06-04 13:36:20 +0300

    minor fixes

--- tugmeteo/__init__.py
+++ tugmeteo/__init__.py
@@ -1,3 +1,28 @@
 #!/usr/bin/env python
 
+"""
+tugmeteo
+
+TÜBİTAK National Observatory (TUG) Meteorology Library
+
+A library where instant and historical meteorological data can be obtained.
+
+TUBITAK National Observatory (TUG) is located at an altitude of 2500 m, on the
+top of Bakirlitepe mountain in Saklikent region of the city of Antalya
+which is located on the Mediterranian coast of Türkiye. It resides in a
+longitudinally important region, because there is no other active observatory
+along and around this longitude on which TUG is located. Besides it fills a
+significant gap, on the map from far east to the west of Europe. It deserves
+to point out that the Observatory site is exceptionally good in terms of
+climatologic conditions.
+
+
+"""
+
+__author__ = 'Oğuzhan OKUYAN'
+__license__ = 'Apache License 2.0'
+__version__ = '1.0.0'
+__maintainer__ = 'Oğuzhan OKUYAN'
+__email__ = 'ookuyan@gmail.com, oguzhan.okuyan@tubitak.gov.tr'
+
 from .core import *
